365a04e9815fe1e76a6c005887b51aafda50c602
zainahmedhussain <zainahmedhussain@gmail.com>
2025-08-13 02:40:04 +1000

    AWS Lambda + API Gateway Hello World

---
+++ lambda_function.py
@@ -0,0 +1,7 @@
+import json
+def lambda_handler(event, context):
+    return {
+        "statusCode": 200,
+        "headers": {"Content-Type": "application/json"},
+        "body": json.dumps({"message": "Hello from Zain's Cloud API!"})
+    }
